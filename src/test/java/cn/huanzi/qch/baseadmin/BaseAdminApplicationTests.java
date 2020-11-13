4a05d6d834a645b9ddcbcf6df86f6aa1c775831d
634026752@qq.com <634026752@qq.com>
2020-11-13 10:39:38 +0800

    Merge branch 'main' of /Volumes/SD/ideaprojects/base-admin with conflicts.

--- src/test/java/cn/huanzi/qch/baseadmin/BaseAdminApplicationTests.java
+++ src/test/java/cn/huanzi/qch/baseadmin/BaseAdminApplicationTests.java
@@ -1,7 +1,11 @@
 package cn.huanzi.qch.baseadmin;
 
+import cn.huanzi.qch.baseadmin.consumer.entity.SysConsumer;
+import cn.huanzi.qch.baseadmin.consumer.service.SysConsumerService;
+import cn.huanzi.qch.baseadmin.consumer.vo.SysConsumerVO;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +13,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class BaseAdminApplicationTests {
 
+    @Autowired
+    private SysConsumerService sysConsumerService;
     @Test
     public void contextLoads() {
+//        System.out.println("213132");
+//        SysConsumerVO sysConsumer = new SysConsumerVO();
+//        sysConsumer.setCName("哈哈哈");
+////        sysConsumerService.save(sysConsumer);
+//        sysConsumerService.save(sysConsumer);
     }
 
 }
